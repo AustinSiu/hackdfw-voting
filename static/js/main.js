04ae55da88c001ca041ab7d765cb4d4cb3ffdcf8
Sami Glasco <samiglasco@gmail.com>
2016-04-16 22:20:14 -0500

    bit of format

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,16 @@
 var ref = new Firebase("https://group-poll.firebaseio.com/");
 
 function createGroup(groupName) {
-    console.log(groupName);
+  console.log(groupName);
 
-    if (groupName != null) {
-	    var newRef = ref.push({
-	      "name": groupName
-	    });
-      var newID = newRef.key();
-      console.log(newID);
-      window.location.replace("/group/" + newID);
-    }
+  if (groupName != null) {
+    var newRef = ref.push({
+      "name": groupName
+    });
+    var newID = newRef.key();
+    console.log(newID);
+    window.location.replace("/group/" + newID);
+  }
 }
 
 function joinGroup(groupID) {
