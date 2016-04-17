d8d0366c6af60782fb5b3f2f4f7ccff598344420
Sami Glasco <samiglasco@gmail.com>
2016-04-17 00:11:19 -0500

    CRY

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,6 @@
 var ref = new Firebase("https://group-poll.firebaseio.com/");
 
+
 function createGroup(groupName) {
 	console.log(groupName);
 
@@ -8,7 +9,7 @@ function createGroup(groupName) {
 			"name": groupName
 		});
 		var newID = newRef.key();
-		console.log(newID);
+		// console.log(newID);
 		window.location.replace("/group/" + newID);
 	}
 }
@@ -23,60 +24,59 @@ function loadItems() {
 	var template = Handlebars.compile(source);
 
 	ref.child(groupKey + "/name").on("value", function (snapshot) {
-		console.log(snapshot.val());
 		$('#GroupName').replaceWith(snapshot.val());
 		// $('#items').append(template(json));
 	});
 
-
-	ref.child(groupKey + "/items").orderByChild("name").on("child_added", function (snapshot) {
+	ref.child(groupKey + "/items").on("child_added", function (snapshot) {
 		var json = snapshot.val();
 		var key = snapshot.key();
 		json.key = key;
-		//console.log(json);
+		console.log(json);
+		console.log(json.name)
 		$('#items').append(template(json));
 	});
 
-	ref.child(groupKey + "/items").on("child_changed", function (snapshot) {
-		var json = snapshot.val();
-		var key = snapshot.key();
-		json.key = key;
-		console.log(json);
-		$('#' + snapshot.key()).replaceWith(template(json));
-		console.log(snapshot.key())
-	});
+	// ref.child(groupKey + "/items").on("child_changed", function (snapshot) {
+	// 	var json = snapshot.val();
+	// 	var key = snapshot.key();
+	// 	json.key = key;
+	// 	console.log(json);
+	// 	$('#' + snapshot.key()).replaceWith(template(json));
+	// 	console.log(snapshot.key())
+	// });
 
-	ref.child(groupKey + "/items").on("child_removed", function (snapshot) {
-		var json = snapshot.val();
-		var key = snapshot.key();
-		json.key = key;
-		console.log(json);
-		$('#' + snapshot.key()).replaceWith();
-		console.log(snapshot.key())
-	});
-}
-
-function updateWeak(key) {
-	ref.child(groupKey + "/items/" + key + "/votes").transaction(function (current_value) {
-		return +current_value + +1;
-	});
-}
-function updateStrong(key) {
-	ref.child(groupKey + "/items/" + key + "/votes").transaction(function (current_value) {
-		return +current_value + +2;
-	});
+	// ref.child(groupKey + "/items").on("child_removed", function (snapshot) {
+	// 	var json = snapshot.val();
+	// 	var key = snapshot.key();
+	// 	json.key = key;
+	// 	console.log(json);
+	// 	$('#' + snapshot.key()).replaceWith();
+	// 	console.log(snapshot.key())
+	// });
 }
 
-function deleteItem(key) {
-	ref.child(groupKey + "/items/" + key).remove();
-}
+// function updateWeak(key) {
+// 	ref.child(groupKey + "/items/" + key + "/votes").transaction(function (current_value) {
+// 		return +current_value + +1;
+// 	});
+// }
+// function updateStrong(key) {
+// 	ref.child(groupKey + "/items/" + key + "/votes").transaction(function (current_value) {
+// 		return +current_value + +2;
+// 	});
+// }
 
-// TODO change this to a modal maybe
-function rename(key) {
-	var newName = prompt("Please enter the new name");
-	if (newName != null) {
-		ref.child(groupKey + "/items/" + key + "/name").transaction(function () {
-			return newName;
-		});
-	}
-}
+// function deleteItem(key) {
+// 	ref.child(groupKey + "/items/" + key).remove();
+// }
+
+// // TODO change this to a modal maybe
+// function rename(key) {
+// 	var newName = prompt("Please enter the new name");
+// 	if (newName != null) {
+// 		ref.child(groupKey + "/items/" + key + "/name").transaction(function () {
+// 			return newName;
+// 		});
+// 	}
+// }
