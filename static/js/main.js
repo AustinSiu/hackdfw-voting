06aff9663a19808246f3c084d45a7d9ac9fe0419
Sharon White <Sharonlulu12@yahoo.com>
2016-04-16 20:21:55 -0500

    changed paths

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+var ref = new Firebase("https://group-poll.firebaseio.com/");
+
+function createGroup(groupName) {
+    console.log(groupName);
+
+    // if (groupName != null) {
+	   //  ref.push({
+	   //    "name": groupName
+	   //  });
+    // }
+}
