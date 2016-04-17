dcf8b90e067372e886ffc07383d30f521622fd36
Sami Glasco <samiglasco@gmail.com>
2016-04-16 22:49:25 -0500

    heroku no work

--- app.py
+++ app.py
@@ -20,5 +20,6 @@ def group_id_path(path):
 	return render_template('vote.html', groupKey = path)
 
 if __name__ == '__main__':
+# app.run()
     port= int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
