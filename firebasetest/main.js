2ee55dd5671e6a1ba859f913c93586e37f6a9b21
Sami Glasco <samiglasco@gmail.com>
2016-04-16 19:37:44 -0500

    update firebase test js

--- firebasetest/main.js
+++ firebasetest/main.js
@@ -8,20 +8,17 @@ function makestuff() {
     //});
     ref.child(groupKey + "/items").push({
         "name": "thing4",
-        "strongVotes": 9,
-        "weakVotes": 1
+        "votes": 0,
     })
 }
 
 function addItem() {
     var name = document.getElementById("name").value;
-    var sVotes = document.getElementById("sVotes").value;
-    var wVotes = document.getElementById("wVotes").value;
+    var votes = document.getElementById("votes").value;
 
     ref.child(groupKey + "/items").push({
         "name": name,
-        "strongVotes": sVotes,
-        "weakVotes": wVotes
+        "votes": votes
     })
 }
 
@@ -30,34 +27,73 @@ function load() {
     var source = $("#some-template").html();
     var template = Handlebars.compile(source);
 
+    //var totalVotes;
+    //
+    //ref.child(groupKey + "/totalVotes").on("value", function (current_value) {
+    //    totalVotes = current_value.val();
+    //});
 
     ref.child(groupKey + "/items").orderByChild("name").on("child_added", function (snapshot) {
         var json = snapshot.val();
         var key = snapshot.key();
         json.key = key;
-
-        console.log(json);
+        //json.percent = ((snapshot.val().votes / totalVotes) * 100);
+        //console.log(json);
         $('#items').append(template(json));
     });
 
-    //ref.child(groupKey + "/items").on("child_changed", function (snapshot) {
-    //    var json = snapshot.val();
-    //    console.log(json);
-    //    $('#items').append(template(snapshot.val()));
-    //    console.log(snapshot.key())
-    //    //update(snapshot.key())
-    //});
+    ref.child(groupKey + "/items").on("child_changed", function (snapshot) {
+        var json = snapshot.val();
+        var key = snapshot.key();
+        json.key = key;
+        //json.percent = ((snapshot.val().votes / totalVotes) * 100);
+        console.log(json);
+        $('#' + snapshot.key()).replaceWith(template(json));
+        console.log(snapshot.key())
+    });
 
+    ref.child(groupKey + "/items").on("child_removed", function (snapshot) {
+        var json = snapshot.val();
+        var key = snapshot.key();
+        json.key = key;
+        console.log(json);
+        $('#' + snapshot.key()).replaceWith();
+        console.log(snapshot.key())
+    });
 }
 
-function update(key){
-    console.log(key);
-    //var hopperRef = ref.child(groupKey + "/items/" + key);
-    //hopperRef.update({
-    //    "weakVotes": 4
+function updateWeak(key) {
+    ref.child(groupKey + "/items/" + key + "/votes").transaction(function (current_value) {
+        return +current_value + +1;
+    });
+
+    //ref.child(groupKey + "/totalVotes").transaction(function (current_value) {
+    //    return +current_value + +1;
+    //});
+}
+function updateStrong(key) {
+    ref.child(groupKey + "/items/" + key + "/votes").transaction(function (current_value) {
+        return +current_value + +2;
+    });
+
+    //ref.child(groupKey + "/totalVotes").transaction(function (current_value) {
+    //    return +current_value + +2;
     //});
 }
 
+function deleteItem(key) {
+    ref.child(groupKey + "/items/" + key).remove();
+}
+
+function rename(key) {
+    var newName = prompt("Please enter the new name");
+
+    if (newName != null) {
+        ref.child(groupKey + "/items/" + key + "/name").transaction(function () {
+            return newName;
+        });
+    }
+}
 
 function loadItems() {
 
