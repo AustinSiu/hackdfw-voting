ecf066b9c4ab6a00230bd7c6c2987ad3f8a51b0a
Austin Siu <Austin Siu>
2016-04-17 04:53:02 -0500

    Can upload image, issues creating new item.

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,8 @@
 var ref = new Firebase("https://group-poll.firebaseio.com/");
 
+
+
+
 function createGroup(groupName) {
     console.log(groupName);
 
@@ -10,5 +13,26 @@ function createGroup(groupName) {
       var newID = newRef.key();
       console.log(newID);
       window.location.replace("/group/" + newID);
+    }
+}
+
+function createItem(itemName, imgName) {
+    console.log(itemName + " " + imgName);
+
+    if (itemName != null) {
+	   var newRef = ref.push({
+	      "name": itemName
+	   });
+       var newID = newRef.key();
+       console.log(newID);
+       window.location.replace("/item/" + newID);
+       if (imgName != null) {
+	       var newImgRef = ref.push({
+	           "name": imgName
+	       });
+           var newImgID = newImgRef.key();
+           console.log(newImgID);
+           window.location.replace("/item/" + newImgID);
+        }
     }
 }
