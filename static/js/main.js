8ba3fa2f3d31f45be528186cf9b76ee9c72a0c49
Sami Glasco <samiglasco@gmail.com>
2016-04-17 01:51:45 -0500

    item updates

--- static/js/main.js
+++ static/js/main.js
@@ -25,58 +25,60 @@ function loadItems() {
 
 	ref.child(groupKey + "/name").on("value", function (snapshot) {
 		$('#GroupName').replaceWith(snapshot.val());
-		// $('#items').append(template(json));
 	});
 
 	ref.child(groupKey + "/items").orderByChild("name").on("child_added", function (snapshot) {
 		var json = snapshot.val();
 		var key = snapshot.key();
 		json.key = key;
-		console.log(json);
-		console.log(json.name)
+		// console.log(json);
+		// console.log(json.name)
 		$('#items').append(template(json));
 	});
 
-	// ref.child(groupKey + "/items").on("child_changed", function (snapshot) {
-	// 	var json = snapshot.val();
-	// 	var key = snapshot.key();
-	// 	json.key = key;
-	// 	console.log(json);
-	// 	$('#' + snapshot.key()).replaceWith(template(json));
-	// 	console.log(snapshot.key())
-	// });
+	ref.child(groupKey + "/items").on("child_changed", function (snapshot) {
+		var json = snapshot.val();
+		var key = snapshot.key();
+		json.key = key;
+		// console.log(json);
+		$('#' + snapshot.key()).replaceWith(template(json));
 
-	// ref.child(groupKey + "/items").on("child_removed", function (snapshot) {
-	// 	var json = snapshot.val();
-	// 	var key = snapshot.key();
-	// 	json.key = key;
-	// 	console.log(json);
-	// 	$('#' + snapshot.key()).replaceWith();
-	// 	console.log(snapshot.key())
-	// });
+		console.log(snapshot.key())
+	});
+
+	ref.child(groupKey + "/items").on("child_removed", function (snapshot) {
+		var json = snapshot.val();
+		var key = snapshot.key();
+		json.key = key;
+		console.log(json);
+		$('#' + snapshot.key()).replaceWith();
+		console.log(snapshot.key())
+	});
 }
 
-// function updateWeak(key) {
-// 	ref.child(groupKey + "/items/" + key + "/votes").transaction(function (current_value) {
-// 		return +current_value + +1;
-// 	});
-// }
-// function updateStrong(key) {
-// 	ref.child(groupKey + "/items/" + key + "/votes").transaction(function (current_value) {
-// 		return +current_value + +2;
-// 	});
-// }
+function updateWeak(key) {
+	ref.child(groupKey + "/items/" + key + "/votes").transaction(function (current_value) {
+		$('#votes+' + key).replaceWith(+current_value + +1);
 
-// function deleteItem(key) {
-// 	ref.child(groupKey + "/items/" + key).remove();
-// }
+		return +current_value + +1;
+	});
+}
+function updateStrong(key) {
+	ref.child(groupKey + "/items/" + key + "/votes").transaction(function (current_value) {
+		return +current_value + +2;
+	});
+}
+
+function deleteItem(key) {
+	ref.child(groupKey + "/items/" + key).remove();
+}
 
 // // TODO change this to a modal maybe
-// function rename(key) {
-// 	var newName = prompt("Please enter the new name");
-// 	if (newName != null) {
-// 		ref.child(groupKey + "/items/" + key + "/name").transaction(function () {
-// 			return newName;
-// 		});
-// 	}
-// }
+function rename(key) {
+	var newName = prompt("Please enter the new name");
+	if (newName != null) {
+		ref.child(groupKey + "/items/" + key + "/name").transaction(function () {
+			return newName;
+		});
+	}
+}
